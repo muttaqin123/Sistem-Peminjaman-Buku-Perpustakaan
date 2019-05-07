656ab4e2e97bf5883a0b7ee8945e0862fdbf501e
yopantumorang <ekoyopan13@gmail.com>
2019-05-07 08:23:49 +0700

    Update DaftarRakBuku.java

--- Buku/DaftarRakBuku.java
+++ Buku/DaftarRakBuku.java
@@ -5,19 +5,20 @@ import InterfacedanAbstract.AmbilData;
 public class DaftarRakBuku extends AmbilData{
 
     RakBuku lemari;
-    
+    String idrak;
     public DaftarRakBuku (){
         lemari= new RakBuku();
     }
     
-    public DaftarRakBuku(RakBuku lemari) {
+    public DaftarRakBuku(String idrak,RakBuku lemari) {
         this.lemari = lemari;
-        
+        this.idrak = idrak;
     }
     @Override
     public boolean validasiData() {
         
         return (
+                !idrak.isEmpty() &&
                 !lemari.getJumlah_bk().isEmpty() &&
                 !lemari.getKode_buku().isEmpty() //&&
                 );
@@ -26,8 +27,9 @@ public class DaftarRakBuku extends AmbilData{
     @Override
     public void setSQL() {
         sql = ("Insert Into rakbuku("
-                + "kodebuku,rakprodi,jumlahbuku)"
+                + "idrak,kodebuku,rakprodi,jumlahbuku)"
                 + " Values("
+                + "'"+idrak+"',"
                 + "'"+lemari.getKode_buku()+"',"
                 +"'"+lemari.getRakbuku()+"',"
                 + "'"+lemari.getJumlah_bk()+"');");    
